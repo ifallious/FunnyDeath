4a6f1e3576f5ef9a262142dfbcc4f7be4065b4a4
ifallious <117840700+ifallious@users.noreply.github.com>
2022-11-13 10:32:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+import Config from "././Config/Config"
+import {prefix, mc} from "./Code/Utils"
+
+const ChatComponentText = Java.type("net.minecraft.util.ChatComponentText");
+register("chat", (ripbozo, eitherwereorwas, killedby) => {
+    let deathmessage = Config.toxicdeathmessage.replace("player", ripbozo)
+    if (ripbozo !== "You") {
+
+    }
+    else {
+        DisplayScreen()
+    }
+}
+).setCriteria(" ☠ ${ripbozo} w${eitherwereorwas} ${killedby} by Voidgloom Seraph.")
+
+function DisplayScreen(){
+    CheatTempBan(getTime())
+}
+
+function getTime(){
+    return "29d 23h 59m 59s"
+}
+
+function CheatTempBan(time){
+    banned = new ChatComponentText("§cYou are temporarily banned for §f" + time + " §cfrom this server!\n\n§7Reason: §rCheating through the use of unfair game advantages.\n§7Find out more: §b§nhttps://www.hypixel.net/appeal\n\n§7Ban ID: §r#" + makeid()+"\n§7Sharing your Ban ID may affect the processing of your appeal!")
+    mc.func_147114_u().func_147298_b().func_150718_a(banned)
+  }
+
+  function makeid() {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789012345678901234567890123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 8; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() *charactersLength));
+   }
+   return result;
+  }  
